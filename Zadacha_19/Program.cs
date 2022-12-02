93a1b781aa5b9c970c518210830a0cd479d541a2
Asus <koncarik@mail.ru>
2022-12-02 23:10:19 +0500

    Zadacha_19

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int DataЕntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int number = DataЕntry("Введите пятизначное число: ");
+
+if (number / 10000 == number % 10 & number / 1000 % 10 == number / 10 % 10)
+    Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Число не является палиндромом");
