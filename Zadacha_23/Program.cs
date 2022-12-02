955e78bf4de403084a573e2d1452a0edeb5e0d4b
Asus <koncarik@mail.ru>
2022-12-02 21:09:06 +0500

    Zadacha_23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+double DataЕntry(string str)
+{
+    Console.Write(str);
+    double number = double.Parse(Console.ReadLine());
+    return number;
+}
+double number = DataЕntry("Введите число: ");
+double count = 1;
+while (count <= number)
+{
+    Console.WriteLine(Math.Pow(count, 3));// Math.Pow((y2 - y1), 2));
+    count++;
+}
